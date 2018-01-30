fca62e5199e09590f6e688cbbc0ab6e1a981214c
root <root@etherpad-lite-1.6>
2018-01-30 08:17:15 +0000

    Implementing pull request 3187 to resolve 'cookies not enabled' issue

--- src/static/js/pad_cookie.js
+++ src/static/js/pad_cookie.js
@@ -23,6 +23,9 @@
 
 var padcookie = (function()
 {
+
+  var cookieName = isHttpsScheme() ? "prefs" : "prefsHttp";
+
   function getRawCookie()
   {
     // returns null if can't get cookie text
@@ -31,7 +34,7 @@ var padcookie = (function()
       return null;
     }
     // look for (start of string OR semicolon) followed by whitespace followed by prefs=(something);
-    var regexResult = document.cookie.match(/(?:^|;)\s*prefs=([^;]*)(?:;|$)/);
+    var regexResult = document.cookie.match(new RegExp("(?:^|;)\\s*" + cookieName + "=([^;]*)(?:;|$)"));
     if ((!regexResult) || (!regexResult[1]))
     {
       return null;
@@ -44,7 +47,7 @@ var padcookie = (function()
     var expiresDate = new Date();
     expiresDate.setFullYear(3000);
     var secure = isHttpsScheme() ? ";secure" : "";
-    document.cookie = ('prefs=' + safeText + ';expires=' + expiresDate.toGMTString() + secure);
+    document.cookie = (cookieName + "=" + safeText + ";expires=" + expiresDate.toGMTString() + secure);
   }
 
   function parseCookie(text)
@@ -122,6 +125,9 @@ var padcookie = (function()
     {
       return wasNoCookie;
     },
+    isCookiesEnabled: function() {
+        return !!getRawCookie();
+    },    
     getPref: function(prefName)
     {
       return cookieData[prefName];
